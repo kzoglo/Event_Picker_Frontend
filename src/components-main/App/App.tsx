352b10852ea19cc62d32f57e8dea77812e745523
Kamil Zoglo <zoglokamil@gmail.com>
2021-09-20 18:37:15 +0200

    Main components: - App component (central point of application)

---
+++ src/components-main/App/App.tsx
@@ -0,0 +1,12 @@
+import EventPicker from '../EventPicker/EventPicker';
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <EventPicker />
+    </div>
+  );
+}
+
+export default App;
