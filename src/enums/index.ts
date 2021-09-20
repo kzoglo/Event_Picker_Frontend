703a01b06e6729bb669ef9cddeee25a8340b91b0
Kamil Zoglo <zoglokamil@gmail.com>
2021-09-20 18:32:43 +0200

    App-wide enums with names of classes for manipulation of HTML elements appearance and state.

---
+++ src/enums/index.ts
@@ -0,0 +1,14 @@
+export enum ClassesToManipulate {
+  INVISIBLE = 'invisible',
+  VISIBLE = 'visible',
+  SHOW = 'show',
+  HIDE = 'hide',
+  GRID = 'grid',
+  SUBMIT_BTN = 'submitBtn',
+  INVALID = 'invalid',
+  VALID = 'valid',
+  NEUTRAL_INPUT = 'neutral-input',
+  INVALID_INPUT = 'invalid-input',
+  CURSOR_AUTO = 'cursor-auto',
+  CURSOR_POINTER = 'cursor-pointer',
+}
